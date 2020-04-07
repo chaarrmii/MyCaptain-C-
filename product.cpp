3db7701b7bd59bbaf552b8acb6c86f0375a568a3
chaarrmii <61911988+chaarrmii@users.noreply.github.com>
2020-04-07 18:14:47 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ float a,b;
+ cout<<"Enter two number.";
+ cin>>a>>b;
+ cout<<"The multiplication is "<<a*b;
+ 
+}
---
+++ task1.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a;
+    int b;
+    float c;
+    double d;
+ cout<<"The size of char is "<<sizeof(a)<<" bytes"<<endl;
+ cout<<"The size of int is "<<sizeof(b)<<" bytes"<<endl;
+ cout<<"The size of float is "<<sizeof(c)<<" bytes"<<endl;
+ cout<<"The size of double is "<<sizeof(d)<<" bytes"<<endl;
+ 
+}
